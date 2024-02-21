76f271fc9e28f609b01191f7cb18c49053c1b745
thetoster <kasper.knapik@gmail.com>
2024-02-21 14:53:23 +0100

    Add logging calculated nicotine amount

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,59 @@
 import "bootstrap";
 import "../scss/style.scss";
 
+const INPUT_REGEX = /\d+/;
+const CALCULATE_FORM_ID = "calculate",
+  BOTTLE_INPUT_ID = "input_bottle",
+  STRENGTH_INPUT_ID = "input_strength",
+  NICOTINE_SHOT_INPUT_ID = "input_nicotine_shot";
+
 window.onload = () => {
-  console.log("site loaded");
+  document
+    .getElementById(CALCULATE_FORM_ID)
+    ?.addEventListener("submit", onSubmit);
 };
+
+/**
+ * Handles submit event when user wants to calculate the data.
+ *
+ * @param {SubmitEvent} event
+ */
+function onSubmit(event) {
+  event.preventDefault();
+
+  /** @type {HTMLFormElement} */
+  let target = event.currentTarget,
+    /** @type {HTMLInputElement} */
+    inputBottle = target.querySelector(`#${BOTTLE_INPUT_ID}`),
+    /** @type {HTMLInputElement} */
+    inputStrength = target.querySelector(`#${STRENGTH_INPUT_ID}`),
+    /** @type {HTMLInputElement} */
+    inputNicotineShot = target.querySelector(`#${NICOTINE_SHOT_INPUT_ID}`);
+
+  let bottleVolume = parseInt(inputBottle.value),
+    nicotineStrength = parseInt(inputStrength.value),
+    nicotineShot = parseInt(inputNicotineShot.value);
+
+  if (!(INPUT_REGEX.test(bottleVolume) && INPUT_REGEX.test(nicotineStrength))) {
+    return null;
+  }
+
+  let calculatedNicotineAmount = calculateNicotineAmount(
+    bottleVolume,
+    nicotineStrength,
+  );
+
+  console.log(`Nicotine amount: ${calculatedNicotineAmount}`);
+}
+
+/**
+ * Calculates how much nicotine needs to be in a bottle to achieve desired nicotineStrength.
+ *
+ * @param {number} bottleVolume Bottle volume in [ml]
+ * @param {number} nicotineStrength Desired nicotine strength in [mg/ml]
+ *
+ * @returns {?number} [mg] of nicotine needed to achieve desired strength; null on error
+ */
+function calculateNicotineAmount(bottleVolume, nicotineStrength) {
+  return bottleVolume * nicotineStrength;
+}
