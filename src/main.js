cec6014545e3913beb1ece8c59efe08a027c2829
thetoster <kasper.knapik@gmail.com>
2024-05-15 19:58:20 +0200

    Rename router for easier readability

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "bootstrap";
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router.js";
 
 const app = createApp(App);
 
