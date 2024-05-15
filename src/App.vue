ca9cd031598e9a31293c6c7824b8a7a9c41c8b4a
thetoster <kasper.knapik@gmail.com>
2024-05-15 19:10:32 +0200

    Add some styling to navbar

--- src/App.vue
+++ src/App.vue
@@ -3,53 +3,62 @@
 </script>
 
 <template>
-  <header>
-    <nav class="navbar navbar-expand-md">
-      <div class="container">
-        <a
-          class="navbar-brand"
-          href="https://github.com/tosterakakwapisz/"
-          target="_blank"
-        >
-          <img
-            class="img-fluid logo"
-            src="https://github.com/tosterakakwapisz.png"
-            alt="Logo"
-          />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <RouterLink :to="{ name: 'home' }" class="nav-link">
-                Home
-              </RouterLink>
-            </li>
-            <li class="nav-item">
-              <RouterLink :to="{ name: 'liquid-mixing' }" class="nav-link">
-                Liquid mixing
-              </RouterLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-
-  <main>
+  <header class="border-bottom">
     <div class="container">
       <div class="row">
-        <div class="col">
+        <div class="col-10 offset-1">
+          <nav class="navbar navbar-expand-md">
+            <div class="container px-0">
+              <a
+                class="navbar-brand"
+                href="https://github.com/tosterakakwapisz/"
+                target="_blank"
+              >
+                <img
+                  class="img-fluid logo"
+                  src="https://github.com/tosterakakwapisz.png"
+                  alt="Logo"
+                />
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <RouterLink :to="{ name: 'home' }" class="nav-link">
+                      Home
+                    </RouterLink>
+                  </li>
+                  <li class="nav-item">
+                    <RouterLink
+                      :to="{ name: 'liquid-mixing' }"
+                      class="nav-link"
+                    >
+                      Liquid mixing
+                    </RouterLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main class="pt-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-10 offset-1">
           <RouterView />
         </div>
       </div>
