472dad850595070b604fd4f1af9053ffcbe7a0ed
thetoster <kasper.knapik@gmail.com>
2024-05-16 12:43:24 +0200

    Add component for Liquid input data

---
+++ src/components/LiquidInputField.vue
@@ -0,0 +1,39 @@
+<template>
+  <label :for="id" class="form-label">{{ description }}</label>
+  <input
+    :value
+    :id
+    type="text"
+    @input="updateInput($event.target.value)"
+    class="form-control"
+  />
+</template>
+
+<script setup>
+  defineProps({
+    id: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+  });
+
+  const emit = defineEmits({
+    "update-input": function (newValue) {
+      return typeof newValue === "number";
+    },
+  });
+
+  /** @param {string} newValue */
+  function updateInput(newValue) {
+    const parsedValue = newValue ? parseInt(newValue) : 0;
+    emit("update-input", parsedValue);
+  }
+</script>
